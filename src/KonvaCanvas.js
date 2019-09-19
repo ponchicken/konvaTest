82edd675793e7651618b448c70f2bcbf763ba787
Alexander Baladin <my@maggit.ru>
2019-09-19 13:41:53 +0300

    Async image load

--- src/KonvaCanvas.js
+++ src/KonvaCanvas.js
@@ -5,43 +5,39 @@ import { cover, contain, downloadFromUrl } from './helpers'
 
 const bg = require('./assets/bg.jpg')
 
-const cWidth = 600
+const cWidth = 700
 const cHeight = 500
 const text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus temporibus, sunt alias dignissimos assumenda ab numquam porro quam consectetur doloremque error praesentium hic doloribus, iusto soluta nulla libero dolore non illo, consequuntur quos. Sed repellendus est dolorem, eum necessitatibus, fugit quasi blanditiis libero ipsum, quod fugiat corrupti assumenda voluptates animi!'
 
 const KonvaCanvas = () => {
   const [stage, setStage] = useState(null)
-
   const id = useMemo(uuid, [])
-  // const layer = useMemo(() => new Konva.Layer(), [])
 
-  const loadImage = () => {
+  const loadImage = async () => {
+    const layer = new Konva.Layer()
+    const imageUrl = bg
+    const imageEl = new window.Image()
+    imageEl.src = `${imageUrl}`
 
-    return new Promise(resolve => {
-      const layer = new Konva.Layer()
-      const imageUrl = bg
-      const imageEl = new window.Image()
-      imageEl.src = `${imageUrl}`
-  
-      imageEl.addEventListener('load', () => {
-        const coverImg =  cover(
-          cWidth,
-          cHeight,
-          imageEl.naturalWidth,
-          imageEl.naturalHeight
-        )
-  
-        const konvaImg = new Konva.Image({
-          ...coverImg,
-          image: imageEl
-        })
-  
-        // setImage(imageEl)
-        layer.add(konvaImg)
-        layer.batchDraw()
-        resolve(layer)
+    imageEl.addEventListener('load', () => {
+      const coverImg =  cover(
+        cWidth,
+        cHeight,
+        imageEl.naturalWidth,
+        imageEl.naturalHeight
+      )
+
+      const konvaImg = new Konva.Image({
+        ...coverImg,
+        image: imageEl
       })
+
+      // setImage(imageEl)
+      layer.add(konvaImg)
+      layer.batchDraw()
     })
+
+    return layer
   }
 
   const loadText = () => {
@@ -53,7 +49,7 @@ const KonvaCanvas = () => {
         "All the world's a stage, and all the men and women merely players. They have their exits and their entrances.",
       fontSize: 24,
       fontFamily: 'Arial',
-      fill: '#999',
+      fill: '#eee',
       width: cWidth,
       padding: 20,
       align: 'left'
@@ -62,9 +58,10 @@ const KonvaCanvas = () => {
     var rect = new Konva.Rect({
       x: 0,
       y: 60,
-      fill: '#ddd',
+      fill: '#000',
       width: cWidth,
-      height: complexText.height()
+      height: complexText.height(),
+      opacity: 0.8
     });
 
     // add the shapes to the layer
@@ -93,7 +90,6 @@ const KonvaCanvas = () => {
 
   const onDownloadClick = () => {
     const url = stage.toDataURL()
-    console.log(url)
     downloadFromUrl({
       url,
       filename: 'image.png'
